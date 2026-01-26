85f29b9cab385d84ead6523b886a101f9a79d8f0
Stoneybro <zionlivingstone4@gmail.com>
2026-01-26 11:29:10 +0100

    feat: Add new blog post explaining cryptography and smart wallets.

--- src/app/blog/posts.ts
+++ src/app/blog/posts.ts
@@ -12,11 +12,18 @@ export const posts: Post[] = [
     slug: "understanding-custom-smart-wallets",
     title: "Understanding Custom Smart Wallets",
     date: "Sep 2025",
-    image:"csm.png",
+    image: "csm.png",
     excerpt:
       "Smart wallets unlock automation and flexibility onchain. This guide covers ERC-4337 and how to build  smart wallets for any use case.",
 
     mdxPath: "understanding-custom-smart-wallets.mdx",
+  },
+  {
+    slug: "cryptography-and-smartwallets",
+    title: "Cryptography and Smart Wallets",
+    date: "Nov 2025",
+    excerpt: "A personal deep dive into Elliptic Curve Cryptography (ECC), Diffie-Hellman key exchange, and how they power modern smart wallets.",
+    mdxPath: "cryptography-and-smartwallets.mdx",
   },
 
 ];
